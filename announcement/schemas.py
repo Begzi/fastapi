782102a17f6a55a94fa0e82d43e8d6bde012fb93
Begzi <49578823+Begzi@users.noreply.github.com>
2023-09-29 10:48:38 +0700

    feat: announcement/schemes

---
+++ announcement/schemas.py
@@ -0,0 +1,31 @@
+from pydantic import BaseModel
+
+
+class AnnouncementCreate(BaseModel):
+    id: int
+    user_id: int
+    title: str
+    subtitle: str
+    text: str
+    is_hidden: bool
+
+
+class AnnouncementUpdate(BaseModel):
+    id: int
+    user_id: int
+    title: str
+    subtitle: str
+    text: str
+    is_hidden: bool
+
+class CommentCreate(BaseModel):
+    id: int
+    announcement_id: int
+    user_id: int
+    text: str
+
+class CommentUpdate(BaseModel):
+    id: int
+    announcement_id: int
+    user_id: int
+    text: str
