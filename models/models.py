b9890d720eec20f79317629dc85348b8fae3b150
Begzi <49578823+Begzi@users.noreply.github.com>
2023-09-29 09:27:51 +0700

    feat: models/models announcement,comment

--- models/models.py
+++ models/models.py
@@ -25,3 +25,27 @@ user = Table(
     Column("is_superuser", Boolean, default=False, nullable=False),
     Column("is_verified", Boolean, default=False, nullable=False),
 )
+
+
+announcement = Table(
+    "announcement",
+    metadata,
+    Column("id", Integer, primary_key=True),
+    Column("user_id", Integer, ForeignKey(user.c.id)),
+    Column("title", String, nullable=False),
+    Column("subtitle", String, nullable=True),
+    Column("text", String, nullable=False),
+    Column("is_hidden", Boolean, default=True, nullable=False),
+    Column("registered_at", TIMESTAMP, default=datetime.utcnow),
+)
+
+
+comment = Table(
+    "comment",
+    metadata,
+    Column("id", Integer, primary_key=True),
+    Column("announcement_id", Integer, ForeignKey(announcement.c.id)),
+    Column("user_id", Integer, ForeignKey(user.c.id)),
+    Column("text", String, nullable=False),
+    Column("registered_at", TIMESTAMP, default=datetime.utcnow),
+)
